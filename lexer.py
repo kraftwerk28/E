e243bf07a3ddf9c827e6a4ccdc379a21cf17fa3f
kraftwerk28 <kefirchik3@gmail.com>
2020-05-19 01:54:29 +0300

    Add dummy parser

--- lexer.py
+++ lexer.py
@@ -20,17 +20,28 @@ class L(Enum):
     TERM = auto()
     IF = auto()
     ELSE = auto()
+    BL = auto()
+    BR = auto()
+    COMMENT = auto()
 
 
 class Lexer:
+    SYMBOLS = {
+        '[': L.SQBL,
+        ']': L.SQBR,
+        '(': L.BL,
+        ')': L.BR,
+    }
+    OPERATORS = ['+', '-', '*', '/']
+
     def __init__(self, filename=None):
         self.fdescriptor = open(filename, 'r') if filename else sys.stdin
         self.char = self.fdescriptor.read(1)
         self.sym = None
         self.line = 1
 
-    def read(self):
-        self.char = self.fdescriptor.read(1)
+    def eat(self, amount=1):
+        self.char = self.fdescriptor.read(amount)
 
     def fail(self, reason):
         print('-> Lexer error:', reason)
@@ -52,75 +63,87 @@ class Lexer:
         ch = self.char
 
         if ch == '\n':
-            self.read()
+            self.eat()
             self.line += 1
             return (L.TERM, None)
 
         elif len(ch) == 0:
             return (L.EOF, None)
 
-        elif ch.isdigit():  # integer / float
+        elif ch.isdigit():  # integer OR float
             s = ''
             while self.char.isdigit() or self.char == '.':
                 s += self.char
-                self.read()
+                self.eat()
             if '.' in s:
                 return (L.FLOAT, float(s))
             else:
                 return (L.INT, int(s))
 
-        elif ch.isspace(): # Pass extra whitespaces
-            self.read()
+        elif ch.isspace():  # Pass out extra whitespaces
+            self.eat()
             return self.read_tok()
 
-        elif ch == '"': # String atom
+        elif ch == '"':  # String atom
             s = ''
-            self.read()
+            self.eat()
             while self.char != '"':
                 s += self.char
-                self.read()
-            self.read()
+                self.eat()
+            self.eat()
             return (L.STRING, s)
 
-        elif ch == '<': # GT operator / assignment
-            self.read()
+        elif ch == '<':  # GT operator OR assignment
+            self.eat()
             if self.char == '-':
-                self.read()
+                self.eat()
                 return (L.CONST_ASS, None)
             elif self.char == '~':
-                self.read()
+                elf.eat()
                 return (L.LET_ASS, None)
             else:
                 return (L.LT, None)
 
         elif ch == '>':
-            self.read()
+            self.eat()
             return (L.GT, None)
 
-        elif ch in ('+', '-', '*', '/'):
+        # Comments processing
+        if ch == '-':
+            self.eat()
+            if self.char == '{':
+                self.eat()
+                s = ''
+                while self.char != '}':
+                    s += self.char
+                    self.eat()
+                self.eat()
+                return (L.COMMENT, s)
+
+            else:
+                return (L.OPERATOR, ch)
+
+        elif ch in self.OPERATORS:
             # TODO: handle // OPERATOR
-            self.read()
+            self.eat()
             return (L.OPERATOR, ch)
 
-        elif ch == '[':
-            self.read()
-            return (L.SQBL, None)
-
-        elif ch == ']':
-            self.read()
-            return (L.SQBR, None)
+        elif ch in self.SYMBOLS:
+            self.eat()
+            return (self.SYMBOLS[ch], None)
 
         elif ch.isalpha():
             s = ''
             while self.char.isalpha():
                 s += self.char
-                self.read()
+                self.eat()
             return (L.ID, None)
 
-        elif ch == '?':  # if-expression
-            self.read()
+        elif ch == '?':  # if-else-expression
+            # TODO: handle infinite else expression
+            self.eat()
             if self.char == ':':
-                self.read()
+                self.eat()
                 return (L.IF, None)
             else:
                 return (L.ELSE, None)
@@ -129,16 +152,12 @@ class Lexer:
             self.fail(f'Unexpected character: [{self.char}]')
 
     def next_token(self) -> (L, Any):
-        # while self.sym == None:
-        #     self.read_tok()
         return self.read_tok()
-
-        # return (sym, value)
 
 
 if __name__ == '__main__':
     l = Lexer('code-samples/simple.xp')
     t = (None, None)
     while t[0] != L.EOF:
-        t = l.read_tok()
+        t = l.next_token()
         print(t)
---
+++ parser.py
@@ -0,0 +1,16 @@
+from lexer import L
+
+
+class Node:
+    pass
+
+class Parser:
+    def __init__(self):
+        pass
+
+    def parse(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
